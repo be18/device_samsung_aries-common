CyanogenMod/android_device_samsung_aries-common

aries-common: add support for froyo secril-client
 * For Vibrant

Change-Id: Idc6c8478fdf4cdae9cd90598838bc93ebc189938

libaudio/Android.mk
@@ -24,6 +24,10 @@ LOCAL_C_INCLUDES += \
 	$(call include-path-for, audio-effects) \
 	$(call include-path-for, audio-utils)
 
+ifeq ($(TARGET_USES_FROYO_RILCLIENT),true)
+  LOCAL_CFLAGS += -DUSES_FROYO_RILCLIENT
+endif
+
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)

be18/device_samsung_aries-common

libaudio/Android.mk
@@ -24,6 +24,10 @@ LOCAL_C_INCLUDES += \
 	$(call include-path-for, audio-effects) \
 	$(call include-path-for, audio-utils)
 
+ifeq ($(BOARD_USES_FROYO_RILCLIENT),true)
+  LOCAL_CFLAGS += -DUSES_FROYO_RILCLIENT
+endif
+
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)